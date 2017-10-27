2d01bffed34d92f5cdc8a9f18abfc3e989f83fb0
Ashlee Krammer <ashlee.alyse@me.com>
2017-10-27 12:23:31 -0400

    Add win/lose funcitons

--- Hangman/Hangman/ViewController.swift
+++ Hangman/Hangman/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var playerOneInstructionLabel: UILabel!
     @IBOutlet weak var playerTwoInstructionLabel: UILabel!
     @IBOutlet weak var hiddenWord: UILabel!
-
+    
     
     //Player Text Fields
     @IBOutlet weak var playerOneTextField: UITextField!
@@ -35,53 +35,87 @@ class ViewController: UIViewController, UITextFieldDelegate {
         super.viewDidLoad()
         playerOneTextField.delegate = self
         playerTwoTextField.delegate = self
-        // Do any additional setup after loading the view, typically from a nib.
+        //Disables Player Two text Field Until Player One Returns
+        //Is this too hard codyy ??
+        playerTwoTextField.isEnabled = false
+        playerOneTextField.isEnabled = true
+        playerOneInstructionLabel.isHidden = false
+        hiddenWord.isHidden = true
+        playerTwoInstructionLabel.isHidden = true
     }
     
-    //should return
+    
+    
+    //When return is pressed
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textFieldArr = [playerOneTextField,playerTwoTextField]
+        textFieldArr = [playerOneTextField.self ,playerTwoTextField.self ]
         print(textFieldArr)
+        
         if textField == playerOneTextField {
             if let str = playerOneTextField.text {
-                model.startGame(str)
                 
+                model.startGame(str)
                 hiddenWord.text = String(repeating: "_ ", count: model.wordToGuess.count)
                 textField.text = ""
                 textField.resignFirstResponder()
                 
+                //Enable and Disable features in view
+                playerOneTextField.isEnabled = false
+                playerOneInstructionLabel.isHidden = true
+                hiddenWord.isHidden = false
+                playerTwoInstructionLabel.isHidden = false
+                playerTwoTextField.isEnabled = true
                 return true
             }
+            
         }
         
         
         if textField == playerTwoTextField {
             if let str = playerTwoTextField.text {
-                if model.yourAreWrong(Character(str)) {
-                    changeImage(model.counter)
-                }
+                if playerTwoTextField.text?.count == 1 {
+                    if model.yourAreWrong(Character(str)) {
+                        changeImage(model.counter)
+                    }
                 
                 displayString = model.displayGuesses(Character(str))
                 hiddenWord.text = displayString
-                //                hiddenWord.text = textField.text
                 textField.text = ""
                 textField.resignFirstResponder()
-                
+                    if model.youWin(){
+                        //disable everything and pop up new view with a restart
+                    } else if model.youLoose() {
+                        //disable but say you lost and allow for a reset as well
+                    }
                 return true
+                }
             }
         }
         
         return false
     }
-
+    
     func changeImage(_ num: Int) {
         let imageName = "man\(num + 1)"
         
         hungManPicture.image = UIImage(named: imageName)
     }
-
+    
     func resetLayout() {
-
+        
+    }
+    func reload() {
+        var message = "Player Wins!"
+        let alert: UIAlertController = UIAlertController(title: "test", message: message, preferredStyle: .alert)
+        let action: UIAlertAction = UIAlertAction(title: "Play Again", style: .cancel, handler: {action in
+            
+            //Put code here that you want to run after the alertte
+            
+        })
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
 
+    
 }
--- Hangman/Hangman/hangmanModel.swift
+++ Hangman/Hangman/hangmanModel.swift
@@ -7,18 +7,22 @@
 //
 
 import Foundation
+import UIKit
 
 class HangmanModel {
     
     var underscoreArr = [Character]()
+    var charBank = [Character]()
     var wordToGuess = ""
     var counter = 0
-    
+
     func startGame(_ wordToGuess: String) {
-        generatePuzzle(wordToGuess)
+        counter = 0
+        generateUnderScores(wordToGuess)
+        
     }
     
-    func generatePuzzle(_ wordToGuess: String) {
+    func generateUnderScores(_ wordToGuess: String) {
         self.wordToGuess = wordToGuess
         underscoreArr = []
         
@@ -42,57 +46,75 @@ class HangmanModel {
     }
     
     
-
+    
     
     
     func yourAreWrong(_ char: Character) -> Bool {
         
         if !Set(wordToGuess).contains(char) {
-            counter += 1
-            return true
+            if !charBank.contains(char){
+                charBank.append(char)
+                print(charBank)
+                counter += 1
+                print(counter)
+                return true
+            }
+            
         }
-        
         return false
     }
     
-//    switch WinOrLose {
-//    case .win:
-//
-//    case .lose:
-//    }
+    func youLoose() -> Bool {
+        if counter == 6 {
+            return true
+        }
+        return false
     
-    
-    
+    }
+        func youWin() -> Bool {
+
+            if !underscoreArr.contains("_") && counter < 6 {
+                return true
+            }
+            return false
+
 }
-    
-    
-//if counter == 7 : you die
-//if arr doesnt contain underscores then you win
 
-//maybe implement the striker on the side
-//enum WinOrLose {
-//    case win, lose
-//}
-
-
-//gamestate func reset() -> Bool {
-//
-//}
-
-//make a var for the wordToGuess.count and have the repeating arr repeat to that count instead of the hard count
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+//    func reload() {
+//        var message = "Player Wins!"
+//        let alert: UIAlertController = UIAlertController(title: "test", message: message, preferredStyle: .alert)
+//        let action: UIAlertAction = UIAlertAction(title: "Play Again", style: .cancel, handler: {action in
+//            
+//          //Put code here that you want to run after the alert
+//            
+//        })
+//        
+//        alert.addAction(action)
+//        present(<#T##viewControllerToPresent: UIViewController##UIViewController#>, animated: <#T##Bool#>, completion: <#T##(() -> Void)?##(() -> Void)?##() -> Void#>)
+//    }
 
 
+
+
+//✓✓✓✓✓✓✓✓✓✓Only allow player two to input a single char ✓
+//create a lose of win func where either win or loose the screen is diabled and asks user if they want to play again // only dif is the label will either say player on/two won
+//maybe make another view that hides during the game and then pops up for lose win condition
+//whats the easiest way to do this UI Style
+
+//win condition // if there are no more underscores, && counter < 6, player two wins
+//if counter first reaches 6, player 1 wins
+
+//the restart needs press button and then to call viewDidLoad and reset the counter
+
+
+
+    
+    
+    
+    
+    
+    
+    
+
+
+}
