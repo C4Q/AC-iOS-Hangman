85d5a9daca660513269569b9c01f7876d27144e0
Richard Crichlow <richardcrichlow@ac.c4q.nyc>
2017-10-28 13:36:39 -0400

    Finished AutoLayout

--- Hangman/Hangman/ViewController.swift
+++ Hangman/Hangman/ViewController.swift
@@ -8,18 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var wordDisplay: UILabel!
+    @IBOutlet weak var messageToPlayer: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if Int(string) != nil {
+            return false
+        }
+        return true
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        guard let text = textField.text else {
+            messageToPlayer.text = "Invalid Input"
+            messageToPlayer.isHidden = false
+            return false
+        }
+        
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        textField.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
+    
+    
+    
 }
 
