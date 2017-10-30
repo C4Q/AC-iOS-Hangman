dc70fbe72a9c6b560606d7c3883315c3bd6935cc
Melissa He <melissahe@ac.c4q.nyc>
2017-10-30 04:02:02 -0400

    Add view controller for player two of two player model

--- Hangman/Hangman/View Controllers/PlayerTwoViewController.swift
+++ Hangman/Hangman/View Controllers/PlayerTwoViewController.swift
@@ -8,12 +8,66 @@
 
 import UIKit
 
-class TwoPlayerPTwoViewController: UIViewController {
+class PlayerTwoViewController: UIViewController, UITextFieldDelegate {
 
+    //Player Two Objects
+    @IBOutlet weak var hangmanImage: UIImageView!
+    @IBOutlet weak var guessedWordLabel: UILabel!
+    @IBOutlet weak var numberGuessesRemaining: UILabel!
+    @IBOutlet weak var gameResultLabel: UILabel!
+    @IBOutlet weak var playerTwoTextField: UITextField!
+    
+    var hangman: HangmanTwoPlayerModel!
+    
     override func viewDidLoad() {
+        guessedWordLabel.text = hangman.guessWord.joined(separator: " ")
+        numberGuessesRemaining.text = "Guesses Remaining:\n\(hangman.numberOfGuessesRemaining)"
+        gameResultLabel.text = "Please enter a letter from the alphabet."
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        playerTwoTextField.delegate = self
+        playerTwoTextField.isUserInteractionEnabled = true
     }
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        guard string.count <= 1, string != "" else {
+            gameResultLabel.text = "Stop trying to delete answers 😒"
+            return false
+        }
+        
+        guard "abcdefghijklmnopqrstuvwxyz".contains(string.lowercased()) else {
+            gameResultLabel.text = "You did not enter a letter 😒"
+            return false
+        }
+        
+        switch hangman.checkLetter(string) {
+        case .correct:
+            guessedWordLabel.text = hangman.guessWord.joined(separator: " ")
+        case .incorrect:
+            gameResultLabel.text = "Incorrect guess."
+            numberGuessesRemaining.text = "Guesses Remaining:\n\(hangman.numberOfGuessesRemaining)"
+        case .used:
+            gameResultLabel.text = "You have used this letter already 😂"
+            return false
+        }
+        
+        switch hangman.checkWin() {
+        case .win:
+            gameResultLabel.text = "Congrats... you got the word right 😒"
+        case .lose:
+            gameResultLabel.text = "Sorry, the correct word was \"\(hangman.randomWord)\".\nBetter luck next time! 😂"
+        case .ongoing:
+            return true
+        }
+        
+        textField.resignFirstResponder()
+        textField.text?.append(string)
+        textField.isUserInteractionEnabled = false
+        return false
+    }
+    
 }
