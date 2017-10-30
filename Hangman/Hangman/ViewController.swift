9425828e894879ccc6d4d6928199253889bea53e
Ashlee Krammer <ashlee.alyse@me.com>
2017-10-30 17:37:57 -0400

    Final Two Player  Hangman

--- Hangman/Hangman/ViewController.swift
+++ Hangman/Hangman/ViewController.swift
@@ -40,35 +40,41 @@ class ViewController: UIViewController, UITextFieldDelegate {
         resetLayout()
         
     }
-    
-    //Func to check if user is inputting letters only
-    func aLetterInAlphabet() -> Bool {
-        if (playerOneTextField.text?.contains(alphabet))!{
-            
-        }
-        if (playerTwoTextField.text?.contains(alphabet))! {
-            
-        }
-        return true
-    }
+
     
     //Func makes sure it is one char and disables backspace
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         
-        
-        if textField == playerTwoTextField {
-            guard let text = playerTwoTextField.text else {
+        //Allows Player One to only enter alphabet letters
+        if textField == playerOneTextField {
+            guard playerOneTextField.text != nil else {
                 return false
             }
-            if !(text.count > 0) {
+            if alphabet.contains(string) {
                 return true
             }
             return false
         }
+        
+         //Allows Player One to only enter alphabet letters and 1 char
+        if textField == playerTwoTextField {
+            guard let text = playerTwoTextField.text else {
+                return false
+            }
+            
+            
+            if alphabet.contains(string) {
+                if !(text.count > 0) {
+                    return true
+                }
+            }
+           
+           return false
+        }
         return true
     }
- 
-   
+    
+    
     
     //When return is pressed, this code runs
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
@@ -78,7 +84,6 @@ class ViewController: UIViewController, UITextFieldDelegate {
         //Player One Entry
         if textField == playerOneTextField {
             if let str = playerOneTextField.text {
-                // if the text field does not equal a num code here
                 model.startGame(str)
                 hiddenWord.text = String(repeating: "_ ", count: model.wordToGuess.count)
                 textField.text = ""
@@ -91,49 +96,41 @@ class ViewController: UIViewController, UITextFieldDelegate {
                 playerTwoInstructionLabel.isHidden = false
                 playerTwoTextField.isEnabled = true
                 return true
-                //}
             }
-            
         }
+        
         
         //Player Two Entry
         
         
         if textField == playerTwoTextField {
             if let str = playerTwoTextField.text {
-                if playerTwoTextField.text?.count == 1 {
-                    // if the text field does not equal a num code here
-                    if model.yourAreWrong(Character(str.lowercased())) {
-                        changeImage(model.counter)
-                    }
-                    
-                    displayString = model.displayGuesses(Character(str.lowercased()))
-                    hiddenWord.text = displayString
-                    textField.text = ""
-                    textField.resignFirstResponder()
-                    if model.youWin(){
-                        winner = "Two"
-                        reload()
-                        
-                    } else if model.youLoose() {
-                        winner = "One"
-                        reload()
-                        
-                    }
-                    return true
+                if model.yourAreWrong(Character(str.lowercased())) {
+                    changeImage(model.counter)
                 }
+                displayString = model.displayGuesses(Character(str.lowercased()))
+                hiddenWord.text = displayString
+                textField.text = ""
+                textField.resignFirstResponder()
+                if model.youWin(){
+                    winner = "Two"
+                    reload()
+                } else if model.youLoose() {
+                    winner = "One"
+                    reload()
+                }
+                return true
             }
         }
-        
         return false
     }
     
-    
+    //Changes image throughout wrong guesses
     func changeImage(_ num: Int) {
         let imageName = "man\(num + 1)"
-        
         hungManPicture.image = UIImage(named: imageName)
     }
+    
     
     //Reset Func
     func resetLayout() {
@@ -146,18 +143,19 @@ class ViewController: UIViewController, UITextFieldDelegate {
         changeImage(0)
     }
     
+    
     //Reload Func
     func reload() {
         let message = "Player \(winner) Wins!"
         let alert: UIAlertController = UIAlertController(title: "Match Finished", message: message, preferredStyle: .alert)
         let action: UIAlertAction = UIAlertAction(title: "Play Again?", style: .cancel, handler: {action in
             self.resetLayout()
-            
         })
-        
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
     }
+    
+    
     
     
 }
