5acfea4b7d531db71e39e2b96259b939959ec662
clint-steve-m <clint_m@clintmejia.com>
2017-10-31 02:39:31 -0400

    third commit

--- Hangman/Hangman/ViewController.swift
+++ Hangman/Hangman/ViewController.swift
@@ -12,38 +12,40 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var mysteryWordLabel: UILabel!
     @IBOutlet weak var potentialLettersLabel: UILabel!
     @IBOutlet weak var gameStatusLabel: UILabel!
-    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var guessingTextField: UITextField!
     @IBOutlet weak var secureTextField: UITextField!
     @IBOutlet weak var hangManAnimation: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.textField.delegate = self
+        self.guessingTextField.delegate = self
         self.secureTextField.delegate = self
         updateGame()
     }
     
     var game = Hangman()
     
+    
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        let characterSet = CharacterSet.letters
-        if string.rangeOfCharacter(from: characterSet.inverted) != nil {
+        if textField == secureTextField {
+            return true
+        }
+        let currentText = textField.text ?? ""
+        guard let stringRange = Range(range, in: currentText) else {
             return false
         }
-        if (textField.text as NSString?)?.replacingCharacters(in: range, with: string).unicodeScalars.count == 1 {
-            return true
+        if textField == guessingTextField && !game.potentialLetters.contains(string) {
+            return false
         }
-        guard textField != secureTextField else  {
-            return true
-        }
-        guard let string = textField.text, potentialLettersLabel.text!.contains(string) else {
-            return true
-        }
-        return true
+        let updatedText = currentText.replacingCharacters(in: stringRange, with: string)
+        return updatedText.count <= 1
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         guard let string = textField.text else {
+            return false
+        }
+        if string == "" {
             return false
         }
         guard textField != secureTextField else {
@@ -93,22 +95,22 @@ class ViewController: UIViewController, UITextFieldDelegate {
         case .victorious:
             mysteryWordLabel.isHidden = true
             potentialLettersLabel.isHidden = true
-            textField.isHidden = true
+            guessingTextField.isHidden = true
             secureTextField.isHidden = true
         case .failed:
             mysteryWordLabel.isHidden = true
             potentialLettersLabel.isHidden = true
-            textField.isHidden = true
+            guessingTextField.isHidden = true
             secureTextField.isHidden = true
         case .onGoing:
             mysteryWordLabel.isHidden = false
             potentialLettersLabel.isHidden = false
-            textField.isHidden = false
+            guessingTextField.isHidden = false
             secureTextField.isHidden = true
         case .enterMysteryWord:
             mysteryWordLabel.isHidden = true
             potentialLettersLabel.isHidden = true
-            textField.isHidden = true
+            guessingTextField.isHidden = true
         }
     }
 }
