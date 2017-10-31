c7d5185cf4a6f646cbc047dbb3083ec4e1eb8237
winstonmaragh@ac.c4q.nyc <WinstonMaragh@ac.c4q.nyc>
2017-10-31 04:12:09 -0400

    updated

---
+++ Hangman/Hangman/HangmanModel.swift
@@ -0,0 +1,58 @@
+//  HangmanModel.swift
+
+import Foundation
+import UIKit
+
+class HangmanModel {
+	var wordString: String = ""
+	var placeholderArray = [Character]()
+	var placeholderString = ""
+	var usedChars: String = ""
+	var wrongGuessCounter: Int = 0
+	var gameOver = false
+	var winLose = ""
+	
+	func resetModel() {
+		wordString = ""
+		placeholderArray = []
+		usedChars = ""
+		wrongGuessCounter = 0
+		winLose = ""
+		gameOver = false
+	}
+	
+	func addWord(_ word: String) {
+		self.wordString = word
+		self.placeholderArray = Array(repeating: "_", count: word.count)
+		self.placeholderString = String(placeholderArray)
+	}
+	
+	func matching(guessLetter: Character) {
+		guard !usedChars.contains(guessLetter) else {return} //guard repeated characters
+		usedChars.append(guessLetter) //add letter to usedChars
+		
+		if wordString.contains(guessLetter) {
+			for (index, value) in wordString.enumerated(){ //iterate through each character in string word
+				if guessLetter == value {//user guessed character matches in word
+					self.placeholderArray[index] = value //update placeholder _ with the letter
+				}
+			}
+			self.placeholderString = String(self.placeholderArray) //assign to string
+			if self.placeholderString == self.wordString {
+				winLose = "You Win!"
+				gameOver = true
+			}
+		} else { //letter does not match in word
+			self.wrongGuessCounter += 1
+			if self.wrongGuessCounter == 7 {
+				winLose = "You Lose"
+				gameOver = true } //if wrong guesses exhausted then end game
+		}
+	}
+}
+	
+	
+	
+
+
+
--- Hangman/Hangman/ViewController.swift
+++ Hangman/Hangman/ViewController.swift
@@ -1,25 +1,86 @@
-//
-//  ViewController.swift
-//  Hangman
-//
-//  Created by Marcel Chaucer on 10/18/17.
-//  Copyright © 2017 Marcel Chaucer. All rights reserved.
-//
+//viewController - Hangman
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+	@IBOutlet weak var enterWord: UITextField!
+	@IBOutlet weak var guessChar: UITextField!
+	@IBOutlet weak var usedLetters: UITextView!
+	@IBOutlet weak var correctLetters: UILabel!
+	@IBOutlet weak var hangmanDisplay: UIImageView!
+	@IBOutlet weak var winLoseDisplay: UILabel!
+	@IBOutlet weak var correctWordDisplay: UILabel!
+	
+	@IBAction func restartGame(_ sender: UIButton) {
+		model.resetModel()
+		enterWord.isUserInteractionEnabled = true
+		enterWord.text = ""
+		guessChar.text = ""
+		usedLetters.text = ""
+		correctLetters.text = ""
+		model.addWord("")
+		hangmanDisplay.image = nil
+		correctWordDisplay.isHidden = true
+		winLoseDisplay.isHidden = true
+		enterWord.becomeFirstResponder() //put cursor at
+	}
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		enterWord.delegate = self
+		guessChar.delegate = self
+		enterWord.becomeFirstResponder()
+		guessChar.isUserInteractionEnabled = false
+	}
+	
+	var model = HangmanModel()
+	
+	func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+		if textField == enterWord {
+			model.addWord(textField.text!.lowercased())
+			correctLetters.text = model.placeholderString
+			enterWord.isUserInteractionEnabled = false
+			guessChar.isUserInteractionEnabled = true
+			guessChar.becomeFirstResponder()
+		}
+		return true
+	}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+	
+	func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+		if textField == guessChar { //check for guessChar textfield
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+			guard string.characters.count == 1 else { return true }//block more than one character
+			guard !model.usedChars.contains(string) else { return true }
+			textField.text = ""
 
+			model.matching(guessLetter: Character(string.lowercased()))
+			usedLetters.text = model.usedChars
+			correctLetters.text = model.placeholderString
 
-}
+			switch model.wrongGuessCounter { //updating hangman
+//			case 0: hangmanDisplay.image = nil
+			case 1: hangmanDisplay.image = #imageLiteral(resourceName: "man1")
+			case 2: hangmanDisplay.image = #imageLiteral(resourceName: "man2")
+			case 3: hangmanDisplay.image = #imageLiteral(resourceName: "man3")
+			case 4: hangmanDisplay.image = #imageLiteral(resourceName: "man4")
+			case 5: hangmanDisplay.image = #imageLiteral(resourceName: "man5")
+			case 6: hangmanDisplay.image = #imageLiteral(resourceName: "man6")
+			case 7: hangmanDisplay.image = #imageLiteral(resourceName: "man7")
+			default: break
+			}//end Switch
+			
+			if model.gameOver {
+				winLoseDisplay.text = model.winLose
+				winLoseDisplay.isHidden = false
+				correctWordDisplay.text = "The correct word was: \(model.wordString) "
+				correctWordDisplay.isHidden = false
+				enterWord.isUserInteractionEnabled = false
+				guessChar.isUserInteractionEnabled = false
+			}
+		} //end if
+		
+		return true
+	}
+} //end class ViewController
 
