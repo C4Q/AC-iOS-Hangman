194841c9ec902eae7254e7ac27db1613bdff7e71
Lisa <imnotlostrui@aim.com>
2017-10-31 10:00:36 -0400

    project

--- Hangman/Hangman/ViewController.swift
+++ Hangman/Hangman/ViewController.swift
@@ -8,18 +8,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var setWordTextField: UITextField!
+    
+    @IBOutlet weak var wordLabel: UILabel!
+    @IBOutlet weak var guessTextField: UITextField!
+    @IBOutlet weak var hangManView: UIView!
+    
+    @IBOutlet weak var guessResultLabel: UILabel!
+    var model = HangManModel()
+    var str = ""
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setWordTextField.delegate = self
+        self.guessTextField.delegate = self
+      
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField.tag == 1 {
+            model.currentWord = [setWordTextField.text!]//current word
+           = model.strArrAsStr()
+            model.wordLines()
+            wordLabel.text = String(model.wordSpacesStorage)
+            //put brain of game here
+        }
+//
+        if textField.tag == 2 {
+            if (guessTextField.text?.count)! > 1 {// if more than 1 char is
+                guessResultLabel.isHidden = false
+                guessResultLabel.text = "1 letter guesses only"
+                //print(textField.text!)
+            }else{//if only 1 letter guess is entered
+                model.guessedLetter = guessTextField.text!
+                guessResultLabel.isHidden = true
+                model.guessedLetter = guessTextField.text!
+                print(model.guessedLetter)
+                model.numOfGuessesLeft -= 1
+                print(model.numOfGuessesLeft)
+               // model.checkGuess
+            }
+           // textField.text = ""//clears text field
+            }
+       
+        textField.resignFirstResponder()
+        return true
     }
-
-
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard string != "" else {
+            //print("must enter a letter")
+            return true
+        }
+        guard CharacterSet.letters.contains(UnicodeScalar(string)!) else {
+            return false
+        }
+        return true
+    }
 }
 
